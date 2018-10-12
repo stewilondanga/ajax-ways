b7998febbdbc8005fc43c7ba2ca89d26570a775d
Stewart ilondanga <stilondanga@gmail.com>
2018-10-12 10:40:05 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
 
-var url = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes';
+/*var url = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes';
 
 //xhr version
 var xhr = document.querySelector("#xhr");
